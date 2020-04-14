fcde45b40b3cd42cbebd16527d3895b1c00c24b9
Le Duc Anh <alexdal1992@gmail.com>
2020-04-14 09:50:37 +0300

    The sixth commit

---
+++ src/components/PostScream.js
@@ -0,0 +1,192 @@
+import React, { Component, Fragment } from "react";
+import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+import MyButton from "../util/MyButton";
+
+// redux components
+import { connect } from "react-redux";
+import { postScream, clearErrors } from "../redux/actions/dataActions";
+
+// material-ui components
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import Dialog from "@material-ui/core/Dialog";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import EditIcon from "@material-ui/icons/Edit";
+import AddIcon from "@material-ui/icons/Add";
+import CloseIcon from "@material-ui/icons/Close";
+
+const styles = {
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#00bcd4",
+      dark: "#008394",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff6333",
+      main: "#ff3d00",
+      dark: "#b22a00",
+      contrastText: "#fff",
+    },
+  },
+  typography: {
+    useNextVariants: true,
+  },
+  form: {
+    textAlign: "center",
+  },
+  image: {
+    margin: "20px auto 20px auto",
+    minWidth: 200,
+  },
+  pageTitle: {
+    margin: "10px auto 10px auto",
+  },
+  textField: {
+    margin: "10px auto 10px auto",
+  },
+  button: {
+    marginTop: 20,
+    position: "relative",
+    float: "right",
+  },
+  customError: {
+    color: "red",
+    fontSize: "0.8rem",
+    marginTop: 10,
+  },
+  progress: {
+    position: "absolute",
+  },
+  card: {
+    display: "flex",
+    marginBottom: 20,
+  },
+  content: {
+    padding: 25,
+    objectFit: "cover",
+  },
+  submitButton: {
+    position: "relative",
+    float: "right",
+  },
+  progressSpinner: {
+    position: "absolute",
+  },
+  closeButton: {
+    position: "absolute",
+    left: "90%",
+    top: "10%",
+  },
+};
+
+class PostScream extends Component {
+  state = {
+    open: false,
+    body: "",
+    errors: {},
+  };
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.UI.errors) {
+      this.setState({
+        errors: nextProps.UI.errors,
+      });
+    }
+    if (!nextProps.UI.errors && !nextProps.UI.loading) {
+      this.setState({ body: "", open: false, errors: {} });
+    }
+  }
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+  handleClose = () => {
+    this.props.clearErrors();
+    this.setState({ open: false, errors: {} });
+  };
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.postScream({ body: this.state.body });
+  };
+  render() {
+    const { errors } = this.state;
+    const {
+      classes,
+      UI: { loading },
+    } = this.props;
+    return (
+      <Fragment>
+        <MyButton onClick={this.handleOpen} tip="Create a post!">
+          <AddIcon />
+        </MyButton>
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          fullWidth
+          maxWidth="sm"
+        >
+          <MyButton
+            tip="Close"
+            onClick={this.handleClose}
+            tipClassName={classes.closeButton}
+          >
+            <CloseIcon />
+          </MyButton>
+          <DialogTitle>Create a new post</DialogTitle>
+          <DialogContent>
+            <form onSubmit={this.handleSubmit}>
+              <TextField
+                name="body"
+                type="text"
+                label="NEW POST"
+                multiline
+                rows="3"
+                placeholder="What's on your mind?"
+                error={errors.body ? true : false}
+                helperText={errors.body}
+                className={classes.textField}
+                onChange={this.handleChange}
+                fullWidth
+              />
+              <Button
+                type="submit"
+                variant="contained"
+                color="primary"
+                className={classes.submitButton}
+                disabled={loading}
+              >
+                Post
+                {loading && (
+                  <CircularProgress
+                    size={30}
+                    className={classes.progressSpinner}
+                  />
+                )}
+              </Button>
+            </form>
+          </DialogContent>
+        </Dialog>
+      </Fragment>
+    );
+  }
+}
+
+PostScream.propTypes = {
+  postScream: PropTypes.func.isRequired,
+  clearErrors: PropTypes.func.isRequired,
+  UI: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  UI: state.UI,
+});
+
+export default connect(mapStateToProps, { postScream, clearErrors })(
+  withStyles(styles)(PostScream)
+);
--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -3,7 +3,11 @@ import {
   LOADING_DATA,
   LIKE_SCREAM,
   UNLIKE_SCREAM,
-  DELETE_SCREAM
+  DELETE_SCREAM,
+  SET_ERRORS,
+  CLEAR_ERRORS,
+  POST_SCREAM,
+  LOADING_UI
 } from "../types";
 import axios from "axios";
 
@@ -24,6 +28,25 @@ export const getScreams = () => dispatch => {
         payload: []
       });
     });
+};
+
+// post a post
+export const postScream = newScream => dispatch => {
+  dispatch({ type: LOADING_UI });
+  axios.post('/scream', newScream)
+  .then(res => {
+    dispatch({
+      type: POST_SCREAM,
+      payload: res.data
+    });
+    dispatch({ type: CLEAR_ERRORS });
+  })
+  .catch(err => {
+    dispatch({
+      type: SET_ERRORS,
+      payload: err.response.data
+    });
+  });
 };
 
 // like a post
@@ -55,7 +78,11 @@ export const unlikeScream = screamID => dispatch => {
 export const deleteScream = ( screamID ) => dispatch => {
   axios.delete(`/scream/${screamID}`)
   .then(() => {
-    dispatch({ type: DELETE_SCREAM, payload: screamID});
+    dispatch({ type: DELETE_SCREAM, payload: screamID });
   })
   .catch(err => console.log(err));
-}
+};
+
+export const clearErrors = () => dispatch => {
+  dispatch({ type: CLEAR_ERRORS });
+};
